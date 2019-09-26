da282108b1fcf124a12bc8e08b543eba4f969841
Achmad Akbar Megantara <48613329+achmegantara@users.noreply.github.com>
2019-09-26 21:26:02 +0700

    Create variabel-tipedata.py

---
+++ basic/variabel-tipedata.py
@@ -0,0 +1,44 @@
+
+#mencetak variabel
+print(variabel)
+print(variabel2)
+print(variabel3)
+
+#delete variabel
+del(variabel)
+
+#mengecek tipe data
+c = type(variabel2)
+d = type(variabel3)
+print(c)
+print(d)
+
+#tipe data angka
+#int dan float
+harga = 12000 #tipe int
+berat = 23.12 #float
+jarak = 3e3 #float 3000.0 , huruf e artinya eksponen 10
+
+print(harga)
+print(berat)
+print(jarak)
+
+#tipe data teks
+#tipe char & tipe string
+nama = "Ega"
+jenis_kelamin = "L"
+alamat = """ Jalan H. Abdul Rahman 27 Sedati Gede, Surabaya """
+agama = 'islam'
+
+print(alamat)
+print(agama) 
+
+#tipe data boolean
+bergerak = True
+nyala = 1
+
+print(bergerak)
+print(nyala)
+
+#konversi tipe data
+# int(), float(), float(), bool(), chr(), str(), bin()
