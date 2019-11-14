48dd58e0fd1b0ee62c743f0b433093892aca8943
Achmad Akbar Megantara <48613329+achmegantara@users.noreply.github.com>
2019-11-14 21:02:18 +0700

    Add files via upload

---
+++ case-problem/add-two-numbers.py
@@ -0,0 +1,9 @@
+#input bilangan
+bil1 = float(input("Masukkan bilangan pertama : "))
+bil2 = float(input("Masukkan bilangan kedua : "))
+
+#menjumlahkan bilangan
+sum = bil1 + bil2
+
+#mencetak hasil
+print("Hasil penjumlah {0} + {1} adalah {2}".format(bil1,bil2,sum))
