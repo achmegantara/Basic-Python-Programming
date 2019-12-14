e4f759991485b90af76c7f8497bd95c0267e4471
Achmad Akbar Megantara <48613329+achmegantara@users.noreply.github.com>
2019-12-14 17:14:45 +0700

    Add files via upload

---
+++ case-problem/Gcd-Fpb-number.py
@@ -0,0 +1,29 @@
+def factornumber(number):
+    for i in range(1,(number+1)):
+        if number%i == 0:
+            factor1.append(i)
+
+def factornumber2(number):
+    for i in range(1,(number+1)):
+        if number%i == 0:
+            factor2.append(i)
+            
+#input bilangan
+bil = int(input("Masukkan bilangan 1 : "))
+bil2 = int(input("Masukkan bilangan 2 : "))
+
+#mencari faktor bilangan
+factor1 = []
+factor2 = []
+factornumber(bil)
+factornumber2(bil2)
+
+#mencari fpb
+temp = []
+for i in range(0,len(factor1)):
+    for j in range(0,len(factor2)):
+        if factor1[i] == factor2[j]:
+            temp.append(factor1[i])
+
+fpb = max(temp)
+print("Maka nilai fpb dari bilangan {0} dan {1} adalah {2}".format(bil,bil2,fpb))
