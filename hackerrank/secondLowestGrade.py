edd4cdfcf8427ffcce6489b2b64f63bfd1b71ece
Achmad Akbar Megantara <achmegantara@gmail.com>
2021-06-04 19:41:16 +0700

    Update secondLowestGrade.py

--- hackerrank/secondLowestGrade.py
+++ hackerrank/secondLowestGrade.py
@@ -1,7 +1,17 @@
-if __name__ == '__main__':
-    for _ in range(int(input())):
-        name = input()
-        score = float(input())
+# if __name__ == '__main__':
+#     for _ in range(int(input())):
+#         name = input()
+#         score = float(input())
 
-print(name)
-print(score)
+# print(name)
+# print(score)
+
+li = [] 
+n = int(input("Enter the number of elements: "))
+for i in range(1, n+1): 
+    elem = int(input("Enter the elements: ")) 
+    li.append(elem) 
+li.sort() 
+
+print("The sorted list: ", li) 
+print("The second smallest value of this list: ",li[1])
