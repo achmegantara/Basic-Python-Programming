c536f708eeebe9c28dc35016afc13169bda49aed
Achmad Akbar Megantara <achmegantara@gmail.com>
2021-07-10 21:47:20 +0700

    Update returnmoney.py

--- hackerrank/btpn test/returnmoney.py
+++ hackerrank/btpn test/returnmoney.py
@@ -1,1 +1,22 @@
+def returnMoney(price, pay):
+    # validation input
+    if price % 1000 == 0 and pay % 100000 == 0 and pay > price:
+        returnPayments = pay - price
+        nomPayments = []
+        nominal = [100000, 20000, 5000, 2000, 1000]
+        i = 0
+        while(i < len(nominal)):
+            if returnPayments >= nominal[i]:
+                nomPayments.append(nominal[i])
+                returnPayments = returnPayments - nominal[i]
+            else:
+                i = i + 1
 
+        print(nomPayments)
+    else:
+        print("Invalid input")
+
+
+price = int(input("Input total price: "))
+pay = int(input("Input total payments: "))
+returnMoney(price, pay)
